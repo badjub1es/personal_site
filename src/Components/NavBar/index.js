9531ca2f2f93691daab32885208ea9b3627aba6f
anthony <theanthonyarellano@gmail.com>
2022-04-28 12:01:04 -0400

    added animation to navbar for one cycle

--- src/Components/NavBar/index.js
+++ src/Components/NavBar/index.js
@@ -1,6 +1,15 @@
+import { useEffect, useState } from 'react';
 import './style/navbar.css';
 
 const NavBar = () => {
+    const [endEvent, setEndEvent] = useState(false);
+
+    useEffect(() => {
+        setTimeout(() => {
+            setEndEvent(() => true)
+        }, 2000)
+    }, [])
+
     return (
         <div>
             <div className='navbar-container'>
@@ -8,18 +17,18 @@ const NavBar = () => {
                     <div style={{marginLeft: "25px"}}>
                         <img
                             alt="logo"
-                            style={{height: "75px", width: "85px"}}
-                            src={require('./style/images/favicon.PNG')}/>
+                            style={{height: "85px", width: "85px"}}
+                            src={endEvent ? require('./style/images/img.PNG') : require('./style/images/anim.gif')}/>
                     </div>
                     {/* logo */}
                 </div>
                 <div className="link-container">
-                    <div className='link fade-in'>About //</div>
-                    <div className='link fade-in2'>Experience //</div>
-                    <div className='link fade-in3'>Projects // </div>
-                    <div className='link fade-in4'>Music //</div>
-                    <div className='link fade-in5'>Contact // </div>
-                    <div className='link fade-in6'>Resume</div>
+                    <div className='link fade-in'>1.About //</div>
+                    <div className='link fade-in2'>2.Experience //</div>
+                    <div className='link fade-in3'>3.Projects // </div>
+                    <div className='link fade-in4'>4.Music //</div>
+                    <div className='link fade-in5'>5.Contact // </div>
+                    <div className='link fade-in6'>6.Resume</div>
                 </div>
             </div>
             <div className='body-container'>
