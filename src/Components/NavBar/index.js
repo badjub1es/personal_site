8ae72f44deb6edcd5e864c97a93aec11263b3a80
anthony <theanthonyarellano@gmail.com>
2022-04-28 11:05:06 -0400

    added fonts, some styling

--- src/Components/NavBar/index.js
+++ src/Components/NavBar/index.js
@@ -1,16 +1,30 @@
+import './style/navbar.css';
+
 const NavBar = () => {
     return (
         <div>
-            <div>
-                {/* logo */}
+            <div className='navbar-container'>
+                <div>
+                    <div style={{marginLeft: "25px"}}>
+                        <img
+                            style={{height: "75px", width: "85px"}}
+                            src={require('./style/images/favicon.PNG')}/>
+                    </div>
+                    {/* logo */}
+                </div>
+                <div className="link-container">
+                    <div className='link'>About</div>
+                    <div className='link'>Experience</div>
+                    <div className='link'>Projects</div>
+                    <div className='link'>Music</div>
+                    <div className='link'>Contact</div>
+                    <div className='link'>Resume</div>
+                </div>
             </div>
-            <div className="link-container">
-                <div>About</div>
-                <div>Experience</div>
-                <div>Projects</div>
-                <div>Contact</div>
-                <div>Resume</div>
+            <div className='body-container'>
+                yes
             </div>
+
         </div>
     )
 };
