7be074dc51e35467e9eb2f93ec0ddc947c8ab33b
anthony <theanthonyarellano@gmail.com>
2022-04-28 19:33:24 -0400

    added links to sites

--- src/Components/NavBar/index.js
+++ src/Components/NavBar/index.js
@@ -1,24 +1,19 @@
-import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import './style/navbar.css';
 
 const NavBar = () => {
-    const [endEvent, setEndEvent] = useState(false);
-
-    useEffect(() => {
-        setTimeout(() => {
-            setEndEvent(() => true)
-        }, 2000)
-    }, [])
 
     return (
         <div>
             <div className='navbar-container'>
                 <div>
-                    <div style={{marginLeft: "25px"}}>
+                    <div style={{ marginLeft: "25px" }}>
                         <img
                             alt="logo"
-                            style={{height: "85px", width: "85px"}}
-                            src={endEvent ? require('./style/images/img.PNG') : require('./style/images/anim.gif')}/>
+                            style={{ height: "85px", width: "85px", cursor: "pointer" }}
+                            onMouseEnter={(e) => e.target.src = require('./style/images/anim.gif')}
+                            onMouseLeave={(e) => e.target.src = require('./style/images/img.PNG')}
+                            src={require('./style/images/img.PNG')} />
                     </div>
                 </div>
                 <div className="link-container">
@@ -32,8 +27,26 @@ const NavBar = () => {
             </div>
             <div className='body-container'>
                 <div className='sidebar'>
-                    <p style={{color: "white"}}>link</p>
-                    <p style={{color: "white"}}>link</p>
+                    <Link to={{ pathname: `https://github.com/badjub1es` }} target="_blank">
+                        <img
+                            alt="github"
+                            className='image-link fade-in4'
+                            src={require('./style/images/github.png')} />
+                    </Link>
+                    <Link to={{ pathname: `https://www.linkedin.com/in/antarellano/` }} target="_blank">
+                        <img
+                            alt="linkedin"
+                            className='image-link fade-in5'
+                            src={require('./style/images/linkedin.png')} />
+                    </Link>
+                    <Link to={{ pathname: `https://www.instagram.com/lowpotassium/` }} target="_blank">
+                        <img
+                            alt="instagram"
+                            style={{ marginBottom: "70px" }}
+                            className='image-link fade-in6'
+                            src={require('./style/images/instagram.png')} />
+                    </Link>
+
                 </div>
             </div>
 
